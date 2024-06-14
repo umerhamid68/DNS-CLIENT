ffbe276ba188873bfa43f43389cc9c37ce26d110
umerhamid68 <umerhamid168@gmail.com>
2024-06-14 15:04:31 +0500

    New code with structural modifications

---
+++ src/CommunicationInterface.ts
@@ -0,0 +1,17 @@
+// export interface CommunicationInterface {
+//     initComm(onResponse: (msg: Buffer) => void): void;
+//     send(packet: Buffer): void;
+//     closeComm(): void;
+// }
+
+import { DNSClient } from "./DNSClient";
+import { OutputLayer } from "./OutputLayer";
+import { PersistenceInterface } from "./PersistanceInterface";
+
+export interface CommunicationInterface {
+    initComm(onResponse: (msg: Buffer) => void, onError: (err: Error) => void): void;
+    send(packet: Buffer): void;
+    closeComm(): void;
+    run(msg: Buffer, persistence: PersistenceInterface, outputLayer: OutputLayer, dnsClient: DNSClient): void;
+}
+
